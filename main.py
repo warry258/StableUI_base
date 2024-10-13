341081944b3168611d7ba06e2fd46ba930fd7598
Marat <148277579+ai-marat@users.noreply.github.com>
2024-10-13 21:07:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ os.makedirs(SAVE_DIR, exist_ok=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load model
-os.system(f'wget -O {MODEL_PATH} "https://civitai.com/api/download/models/627153?type=Model&format=SafeTensor&token=0bb82b0986121fdf354c8a5f0fcca014"')
+os.system(f'wget -O {MODEL_PATH} "https://civitai.com/api/download/models/128078?type=Model&format=SafeTensor&size=pruned&fp=fp16"')
 
 pipe = StableDiffusionXLPipeline.from_single_file(MODEL_PATH, use_safetensors=True, torch_dtype=torch.float16).to(device)
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
